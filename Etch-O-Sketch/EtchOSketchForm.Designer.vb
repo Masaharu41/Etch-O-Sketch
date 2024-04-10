379cf4213b0211421650212dc81abae6ae1635d2
Owen Fujii <dalekrebel@gmail.com>
2024-04-10 11:17:35 -0600

    Add project files.

---
+++ Etch-O-Sketch/EtchOSketchForm.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EtchOSketchForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.LemonChiffon
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.PictureBox1.Location = New System.Drawing.Point(76, 67)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(913, 509)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'EtchOSketchForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Firebrick
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1058, 653)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "EtchOSketchForm"
+        Me.Text = "Etch O Sketch"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Etch-O-Sketch/EtchOSketchForm.vb
@@ -0,0 +1,42 @@
+﻿'Owen Fujii
+'RCET 2265
+'Spring 2024
+'Etch-O-Sketch
+'
+
+
+
+
+Public Class EtchOSketchForm
+
+    Sub mousedraw(x As Integer, y As Integer, updateCord As Boolean)
+        Dim g As Graphics = PictureBox1.CreateGraphics
+        Dim pen As New Pen(Color.Black, 5)
+        Static oldX As Integer, oldY As Integer
+        If updateCord = False Then
+            'oldX = x
+            g.DrawLine(pen, oldX, oldY, x, y)
+            ' oldY = y
+
+        End If
+        oldX = x
+        oldY = y
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        Me.Text = $"({e.X},{e.Y} Button: {e.Button})"
+        'mousedraw(e.X, e.Y)
+        If e.Button = MouseButtons.Left Then
+            mousedraw(e.X, e.Y, False)
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        Me.Text = $"({e.X},{e.Y} Button: {e.Button})"
+        If e.Button = MouseButtons.Left Then
+            mousedraw(e.X, e.Y, True)
+        End If
+    End Sub
+End Class
