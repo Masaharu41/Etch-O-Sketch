f832ccc897092be3edf4ac357932c32cb53d022b
Owen Fujii <dalekrebel@gmail.com>
2024-04-12 09:15:59 -0600

    Added Color Change Functions

--- Etch-O-Sketch/EtchOSketchForm.Designer.vb
+++ Etch-O-Sketch/EtchOSketchForm.Designer.vb
@@ -22,19 +22,97 @@ Partial Class EtchOSketchForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.components = New System.ComponentModel.Container()
+        Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
+        Me.EtchyContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CursorColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawingPadColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawWaveformsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.EtchyContextMenuStrip.SuspendLayout()
         Me.SuspendLayout()
         '
-        'PictureBox1
+        'DrawingPictureBox
         '
-        Me.PictureBox1.BackColor = System.Drawing.Color.LemonChiffon
-        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Cross
-        Me.PictureBox1.Location = New System.Drawing.Point(76, 67)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(913, 509)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
+        Me.DrawingPictureBox.BackColor = System.Drawing.Color.LemonChiffon
+        Me.DrawingPictureBox.ContextMenuStrip = Me.EtchyContextMenuStrip
+        Me.DrawingPictureBox.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.DrawingPictureBox.Location = New System.Drawing.Point(59, 29)
+        Me.DrawingPictureBox.Name = "DrawingPictureBox"
+        Me.DrawingPictureBox.Size = New System.Drawing.Size(913, 509)
+        Me.DrawingPictureBox.TabIndex = 0
+        Me.DrawingPictureBox.TabStop = False
+        '
+        'EtchyContextMenuStrip
+        '
+        Me.EtchyContextMenuStrip.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.EtchyContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ColorsToolStripMenuItem})
+        Me.EtchyContextMenuStrip.Name = "EtchyContextMenuStrip"
+        Me.EtchyContextMenuStrip.Size = New System.Drawing.Size(129, 80)
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(128, 38)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(184, 44)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'ColorsToolStripMenuItem
+        '
+        Me.ColorsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorToolStripMenuItem, Me.DrawWaveformsToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.ColorsToolStripMenuItem.Name = "ColorsToolStripMenuItem"
+        Me.ColorsToolStripMenuItem.Size = New System.Drawing.Size(128, 38)
+        Me.ColorsToolStripMenuItem.Text = "Edit"
+        '
+        'SelectColorToolStripMenuItem
+        '
+        Me.SelectColorToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CursorColorToolStripMenuItem, Me.DrawingPadColorToolStripMenuItem, Me.BackgroundColorToolStripMenuItem})
+        Me.SelectColorToolStripMenuItem.Name = "SelectColorToolStripMenuItem"
+        Me.SelectColorToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.SelectColorToolStripMenuItem.Text = "Select Color"
+        '
+        'CursorColorToolStripMenuItem
+        '
+        Me.CursorColorToolStripMenuItem.Name = "CursorColorToolStripMenuItem"
+        Me.CursorColorToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.CursorColorToolStripMenuItem.Text = "Cursor Color"
+        '
+        'DrawingPadColorToolStripMenuItem
+        '
+        Me.DrawingPadColorToolStripMenuItem.Name = "DrawingPadColorToolStripMenuItem"
+        Me.DrawingPadColorToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.DrawingPadColorToolStripMenuItem.Text = "Drawing Pad Color"
+        '
+        'BackgroundColorToolStripMenuItem
+        '
+        Me.BackgroundColorToolStripMenuItem.Name = "BackgroundColorToolStripMenuItem"
+        Me.BackgroundColorToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.BackgroundColorToolStripMenuItem.Text = "Background Color"
+        '
+        'DrawWaveformsToolStripMenuItem
+        '
+        Me.DrawWaveformsToolStripMenuItem.Name = "DrawWaveformsToolStripMenuItem"
+        Me.DrawWaveformsToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.DrawWaveformsToolStripMenuItem.Text = "Draw Waveforms"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.ClearToolStripMenuItem.Text = "Clear"
         '
         'EtchOSketchForm
         '
@@ -43,13 +121,26 @@ Partial Class EtchOSketchForm
         Me.BackColor = System.Drawing.Color.Firebrick
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1058, 653)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ContextMenuStrip = Me.EtchyContextMenuStrip
+        Me.Controls.Add(Me.DrawingPictureBox)
         Me.Name = "EtchOSketchForm"
         Me.Text = "Etch O Sketch"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.EtchyContextMenuStrip.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents DrawingPictureBox As PictureBox
+    Friend WithEvents EtchyContextMenuStrip As ContextMenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawWaveformsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CursorColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawingPadColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BackgroundColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorDialog As ColorDialog
 End Class
--- Etch-O-Sketch/EtchOSketchForm.vb
+++ Etch-O-Sketch/EtchOSketchForm.vb
@@ -4,14 +4,21 @@
 'Etch-O-Sketch
 '
 
-
+Option Explicit On
+Option Strict On
 
 
 Public Class EtchOSketchForm
 
+    Private Sub LoadDefaults(sender As Object, e As EventArgs) Handles Me.Load
+        ForegroundColor(Color.Black, True)
+        BackColor = BackgroundColorChange(Color.Firebrick, True)
+        DrawingPictureBox.BackColor = PictureForegroundColor(Color.LemonChiffon, True)
+    End Sub
+
     Sub mousedraw(x As Integer, y As Integer, updateCord As Boolean)
-        Dim g As Graphics = PictureBox1.CreateGraphics
-        Dim pen As New Pen(Color.Black, 5)
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics
+        Dim pen As New Pen(ForegroundColor(, False), 5)
         Static oldX As Integer, oldY As Integer
         If updateCord = False Then
             'oldX = x
@@ -25,7 +32,43 @@ Public Class EtchOSketchForm
         g.Dispose()
     End Sub
 
-    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+    Function ForegroundColor(Optional newColor As Color = Nothing, Optional update As Boolean = False) As Color
+        Static newColorVar As Color
+
+        If update Then
+            newColorVar = newColor
+
+        End If
+
+        Return newColorVar
+
+    End Function
+    Function BackgroundColorChange(Optional newColor As Color = Nothing, Optional update As Boolean = False) As Color
+        Static newColorVar As Color
+
+        If update Then
+            newColorVar = newColor
+
+        End If
+
+        Return newColorVar
+
+    End Function
+
+    Function PictureForegroundColor(Optional newcolor As Color = Nothing, Optional update As Boolean = False) As Color
+        Static newColorVar As Color
+
+        If update Then
+            newColorVar = newcolor
+
+        End If
+        Return newColorVar
+
+    End Function
+
+
+    Private Sub DrawingPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseMove
+        'Remove Text for Full submission
         Me.Text = $"({e.X},{e.Y} Button: {e.Button})"
         'mousedraw(e.X, e.Y)
         If e.Button = MouseButtons.Left Then
@@ -33,10 +76,31 @@ Public Class EtchOSketchForm
         End If
     End Sub
 
-    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+    Private Sub DrawingPictureBox_MouseDown(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseDown
+        'Remove Text for Full submission
         Me.Text = $"({e.X},{e.Y} Button: {e.Button})"
         If e.Button = MouseButtons.Left Then
             mousedraw(e.X, e.Y, True)
         End If
+    End Sub
+
+    Private Sub CursorColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CursorColorToolStripMenuItem.Click
+        If ColorDialog.ShowDialog() = DialogResult.OK Then
+            ForegroundColor(ColorDialog.Color, True)
+        End If
+    End Sub
+
+    Private Sub DrawingPadColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DrawingPadColorToolStripMenuItem.Click
+        If ColorDialog.ShowDialog() = DialogResult.OK Then
+            PictureForegroundColor(ColorDialog.Color, True)
+        End If
+        DrawingPictureBox.BackColor = PictureForegroundColor(, False)
+    End Sub
+
+    Private Sub BackgroundColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BackgroundColorToolStripMenuItem.Click
+        If ColorDialog.ShowDialog() = DialogResult.OK Then
+            BackgroundColorChange(ColorDialog.Color, True)
+        End If
+        BackColor = BackgroundColorChange(, False)
     End Sub
 End Class
